bbbb8696af5f2ac8b189d51018c4d834ebcda5c5
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-11-21 22:56:32 -0300

    feat: adjust scope

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -10,12 +10,15 @@ export function SignIn() {
   const githubUrl = new URL('login/oauth/authorize', 'https://github.com/')
   githubUrl.searchParams.set('client_id', import.meta.env.VITE_GITHUB_CLIENT_ID)
 
-  const googleUrl = new URL('https://accounts.google.com/o/oauth2/v2/auth')
+  const googleUrl = new URL('o/oauth2/v2/auth', 'https://accounts.google.com/')
   googleUrl.searchParams.set('client_id', import.meta.env.VITE_GOOGLE_CLIENT_ID)
   googleUrl.searchParams.set('access_type', 'offline')
   googleUrl.searchParams.set('prompt', 'consent')
   googleUrl.searchParams.set('response_type', 'code')
-  googleUrl.searchParams.set('scope', 'email profile')
+  googleUrl.searchParams.set(
+    'scope',
+    'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
+  )
   googleUrl.searchParams.set(
     'redirect_uri',
     `${import.meta.env.VITE_FRONTEND_URL}/auth/google/callback`,
